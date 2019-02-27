0f68a539e13b24ec3c3f576ddbf93a2100af6950
Matt Basile <matthewbasile2@gmail.com>
2019-02-27 15:46:30 -0500

    adding content before restarting

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const knex = require('knex');
-const knexConfig = require('./knexfile')
-const db = knex(knexConfig.development);
+// const knex = require('knex');
+// const knexConfig = require('./knexfile')
+// const db = knex(knexConfig.development);
+const Users = require('./helpers/users-model.js');
 
 const secret = process.env.JWT_SECRET || 'demo secret MFers'; 
 const server = express();
@@ -19,6 +20,20 @@ server.use(cors());
 server.get('/', (req, res) => {
     res.send("sanity check")
 });
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    // generate hash from user's password
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    // override user.password with hash
+    user.password = hash;
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
