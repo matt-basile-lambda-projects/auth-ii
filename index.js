c84ef51eb7dfe47e1de66d398e9af7a2020b5dca
Matt Basile <matthewbasile2@gmail.com>
2019-02-27 15:52:55 -0500

    Logging in and generating Token value

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-server.get('/', (req, res) => {
-    res.send("sanity check")
-});
+// Auth/Login API
 server.post('/api/register', (req, res) => {
     let user = req.body;
     // generate hash from user's password
@@ -33,7 +30,59 @@ server.post('/api/register', (req, res) => {
       .catch(error => {
         res.status(500).json(error);
       });
-  });
+});
+//Generate Token
+function generateToken(user){
+  const payload = {
+      subject: user.id,
+      username: user.username,
+      department: user.department
+  };
+  const options ={
+      expiresIn: '1d',
+  };
+  return jwt.sign(payload, secret, options)
+}
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // generate token
+        const token = generateToken(user)
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}!, have a token...`, token });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+
+
+
+
+
+
+
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json({users});
+    })
+    .catch(err => res.send(err));
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+//Server Starter Sanity Check
+// server.get('/', (req, res) => {
+//     res.send("sanity check")
+// });
